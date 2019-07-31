da6c95baa85f838c038fce1166b815f76b545d81
Yenisbel Valle <yenisbelv@gmail.com>
2019-07-31 14:49:07 -0700

    Updated last exercises

--- BuildingBlock.js
+++ BuildingBlock.js
@@ -54,53 +54,77 @@ console.log(convertToKilometers(361)); //580.973
 // number is between 0-0.65, return "Horrible freethrow shooter"
 //number is between 0.65-0.80 return "Decent freethrow shooter"
 //number is between 0.80-1.00 return "Great freethrow shooter"
-//Each range is inclusive of the lower bound, and exclusive of the upper -- in other words, 0.65 is counted as a "Decent freethrow shooter" (not "Horrible freethrow shooter") and 0.80 is considered a "Great freethrow shooter" (not "Decent freethrow shooter"). Additionally, you can get a number as high as 0.99, but never 1 (this is what the original problem statement means by "exclusive of 1").
+//Each range is inclusive of the lower bound, and exclusive of the upper 
 
 function isGoodFreethrowShooter(number){
-
+  if (0 < number < 0.65) return "Horrible freethrow shooter";
+  if (0.65 <= number < 0.80) return "Decent freethrow shooter";
+  if (0.80 <= number < 1.00) return "Great freethrow shooter";
 }
 
 // isGoodFreethrowShooter(0.90); //"Great freethrow shooter"
 // isGoodFreethrowShooter(0.09); //"Horrible freethrow shooter"
  console.log(isGoodFreethrowShooter(0.55)); //"Decent freethrow shooter"
 
-// // Exercise 6 (strings)
-// // Write a function dogsIWouldPet which takes in an item (string), and returns a sentence stating dogs you would pet in comparison to the item (see sample call below).
+// Exercise 6 (strings)
+// Write a function dogsIWouldPet which takes in an item (string), and returns a sentence stating dogs you would pet
 
-// dogsIWouldPet("ottoman"); //"I would pet dogs no bigger than an ottoman"
-// dogsIWouldPet("small horse"); //"I would pet dogs no bigger than an small horse"
-// dogsIWouldPet("Terrier"); //"I would pet dogs no bigger than an Terrier"
-// Bonus (extra): If your function were passed in a string "I do not like dogs", you can have your function return "I would not pet dogs".
+function dogsIWouldPet(string) {
+  return string === 'I do not like dogs' ? `I would not pet dogs`:`I would pet dogs no bigger than an ${string}`
+}
+console.log(dogsIWouldPet("ottoman")); //"I would pet dogs no bigger than an ottoman"
+console.log(dogsIWouldPet("small horse"));
+console.log(dogsIWouldPet("I do not like dogs"));
+// If your function were passed in a string "I do not like dogs", you can have your function return "I would not pet dogs".
 
-// // Exercise 7 (numbers and conditionals)
-// // Write a function isTallEnough which takes in a height (number) in inches, and returns true if the input is greater than or equal to 48, and false if it's less than 48 inches.
+// Exercise 7 (numbers and conditionals)
+// function which takes in a height (number) in inches, and returns true or falsei f the input is greater than or equal to 48
+function isTallEnough (height) {
+  return height >=48 ? true : false
+}
 
-// isTallEnough(72); //true
-// // isTallEnough(28); //false
+console.log(isTallEnough(72)); //true
+console.log(isTallEnough(28)); //false
 
-// // Exercise 8 (objects)
-// // Write a function favoriteDessert which takes in no arguments, but returns an object with 3 key-value pairs. The names of each property and their respective values are up to you, but the values cannot be undefined. How would you describe your favorite dessert?
+// Exercise 8 (objects)
+// Write a function favoriteDessert which no arguments, but returns an object with 3 key-value pairs.
 
-// // For example, one could return an object such as:
+function Dessert(name, color, savor) {
+  this.name = name;
+  this.color = color;
+  this.savor = savor
+}
 
-// // { name: "Flan", ovenTemp: 350, prepTime: "25 minutes + 50 minutes chilling" }
-// // Another example could be:
+let flan = new Dessert('Flan', 'Brown', 'Sweet')
+console.log(flan)
+let brownie = new Dessert('Chocolate Brownie', 'Brown', 'Sweet')
+console.log(typeof brownie)
 
-// // { name: "Gelato", countryOfOrigin: "Italy", flavor: "Chocolate Chip" }
-// // Get creative!
+// Exercise 9 (loops)
+// Write a function countNumOfStrings which takes in an array and returns the number of strings there are in the array.
 
-// // Exercise 9 (loops)
-// // Write a function countNumOfStrings which takes in an array of mixed elements (different data types), and returns the number of strings there are in the array.
+function countNumOfStrings(array) {
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    let element = array[i];
+    if(typeof element === 'string'){
+      count ++
+    };
+  }
+  return count
+}
 
-
-
-// countNumOfStrings(["hello", "world", 5, true, {}]); //2
+console.log(countNumOfStrings(["hello", "world", 5, true, {}])); //2
 // countNumOfStrings(["foo", 2, 10, "bar", 5, false, {}, "baz"]); //3
-// countNumOfStrings(["There is only one string in this example"]); //1
-// // Hint: Recall typeof? If not: check it out on MDN. :-)
+console.log(countNumOfStrings(["There is only one string in this example"])); //1
 
-// // Exercise 10 (loops)
-// // Write a function listLengthOfAllWords which takes in an array of words (strings), and returns an array of numbers representing the length of each word.
+// Exercise 10 (loops)
+// Write a function listLengthOfAllWords which takes in an array of words (strings), and returns an array of numbers representing the length of each word.
 
-
-// listLengthOfAllWords(['hello', 'world', 'I', 'can', 'code']); //[5, 5, 1, 3, 4];
+function listLengthOfAllWords(array) {
+  for (let j = 0; j < array.length; j++) {
+    array[j] = array[j].length ;
+  };
+  return array
+}
+console.log(listLengthOfAllWords(['hello', 'world', 'I', 'can', 'code'])); //[5, 5, 1, 3, 4];
